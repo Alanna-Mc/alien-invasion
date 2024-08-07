1618d16b60bfdc80af1a17e7d900d7f6a4889c70
= <Alanna-Mc@users.noreply.github.com>
2024-08-07 17:52:57 +0100

    Update game logic and added ship movement functionality

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialise the game's settings."""
+        # Screen settings
+        self.ship_speed = 1.5
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (10, 10, 50)  # Very dark blue
+
+        
