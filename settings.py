21e58ed29df54e897b57545e41a8c9df27de6e59
= <Alanna-Mc@users.noreply.github.com>
2024-08-11 19:51:41 +0100

    Created bullet handling and set a fixed screen size

--- settings.py
+++ settings.py
@@ -9,4 +9,11 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (10, 10, 50)  # Very dark blue
 
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 5
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 10
+
         
