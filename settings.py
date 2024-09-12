d7aaf710857daba37d9fc25533bf7e448bc7fd43
= <Alanna-Mc@users.noreply.github.com>
2024-09-12 15:07:08 +0100

    Added collision detection to the game

--- settings.py
+++ settings.py
@@ -4,13 +4,16 @@ class Settings:
     def __init__(self):
         """Initialise the game's settings."""
         # Screen settings
-        self.ship_speed = 1.5
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (10, 10, 50)  # Very dark blue
 
+        # Ship settings
+        self.ship_speed = 1.5
+        self.ship_limit = 3
+
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -21,6 +24,8 @@ class Settings:
         self.fleet_drop_speed = 10
         # Fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+        
         
 
         
